c1d7b7c2b866495b07196dae5ae2411887eefe3e
Giovanni Ramos <giovannilauro@hotmail.com>
2012-07-20 16:53:15 -0300

    Small update: PDOConfig.class.php

--- classes/PDOConfig.class.php
+++ classes/PDOConfig.class.php
@@ -3,21 +3,20 @@
 /**
  * Interface de configuração do servidor
  * 
- * PHP version 5
- * 
- * @category PHP
+ * @category Config
  * @package PDO4You
  * @author Giovanni Ramos <giovannilauro@gmail.com>
- * @copyright Giovanni Ramos
+ * @copyright 2010-2012, Giovanni Ramos
  * @since 2010-09-07
- * @version 2.0
- * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * @link https://github.com/giovanniramos/PDO4YOU
+ * @version 2.1
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @link https://github.com/giovanniramos/PDO4You
  * 
  * */
 interface PDOConfig
 {
     // Configuração de conexão
+    const DATA_TYPE = 'mysql';
     const DATA_HOST = 'localhost';
     const DATA_PORT = '3306';
     const DATA_USER = 'root';
@@ -30,8 +29,8 @@ interface PDOConfig
     // FireAlert: Habilita o monitoramento de erros no sistema, enviando alertas via e-mail para o Administrador
     const FIREALERT = false;
 
-    // E-mail do Administrador do sistema para receber as notificações do FireAlert
-    const WEBMASTER = 'username@gmail.com';
+    // E-mail do administrador do sistema para receber as notificações do FireAlert
+    const WEBMASTER = 'pdo4you@gmail.com';
 
 }
 
