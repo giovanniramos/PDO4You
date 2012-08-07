0d959534c2ab972f173c621783b955ef0629869f
Giovanni Ramos <giovannilauro@hotmail.com>
2012-08-07 16:27:03 -0400

    Update data/databases.sql

--- data/databases.sql
+++ data/databases.sql
@@ -3,13 +3,12 @@ CREATE DATABASE IF NOT EXISTS pdo4you;
 USE pdo4you;
 
 DROP TABLE IF EXISTS  pdo4you.users;
-CREATE TABLE IF NOT EXISTS pdo4you.users (
-	id int(11) NOT NULL PRIMARY KEY auto_increment,
-	firstname varchar(20) NOT NULL,
-	lastname varchar(20) NOT NULL,
-	mail varchar(30) NOT NULL,
-	datecreate timestamp NOT NULL default CURRENT_TIMESTAMP,
-	status char(1) NOT NULL default '1'
+CREATE TABLE pdo4you.users (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`firstname` VARCHAR(20) NOT NULL,
+	`lastname` VARCHAR(20) NOT NULL,
+	`mail` VARCHAR(30) NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 INSERT INTO pdo4you.users (firstname, lastname, mail) VALUES
@@ -17,13 +16,15 @@ INSERT INTO pdo4you.users (firstname, lastname, mail) VALUES
 
 DROP TABLE IF EXISTS  pdo4you.books;
 CREATE TABLE pdo4you.books (
-	id int(11) NOT NULL PRIMARY KEY auto_increment,
-	title varchar(50) NOT NULL,
-	author varchar(30) NOT NULL
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(50) NOT NULL,
+	`author` VARCHAR(30) NOT NULL,
+	`description` TINYTEXT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 INSERT INTO pdo4you.books VALUES
-(1, 'Learning PHP, MySQL, and JavaScript', 'Robin Nixon');
+(1, 'Learning PHP, MySQL, and JavaScript', 'Robin Nixon', NULL);
 
 
 
@@ -33,17 +34,19 @@ USE bookstore;
 
 DROP TABLE IF EXISTS  bookstore.books;
 CREATE TABLE bookstore.books (
-	id int(11) NOT NULL PRIMARY KEY auto_increment,
-	title varchar(50) NOT NULL,
-	author varchar(30) NOT NULL
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(50) NOT NULL,
+	`author` VARCHAR(30) NOT NULL,
+	`description` TINYTEXT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 INSERT INTO bookstore.books VALUES
-(1, 'Head First PHP & MySQL', 'Lynn Beighley, Michael Morrison'),
-(2, 'Head First JavaScript ', 'Michael Morrison'),
-(3, 'Head First Ajax', 'Rebecca M. Riordan'),
-(4, 'Head First jQuery', 'Ryan Benedetti'),
-(5, 'Head First Java, 2nd Edition', 'Kathy Sierra'),
-(6, 'Head First Python', 'Paul Barry'),
-(7, 'Head First Networking', 'Al Anderson'),
-(8, 'Head First Web Design', 'Ethan Watrall');
+(1, 'Head First PHP & MySQL', 'Lynn Beighley, Michael Morrison', NULL),
+(2, 'Head First JavaScript ', 'Michael Morrison', NULL),
+(3, 'Head First Ajax', 'Rebecca M. Riordan', NULL),
+(4, 'Head First jQuery', 'Ryan Benedetti', NULL),
+(5, 'Head First Java, 2nd Edition', 'Kathy Sierra', NULL),
+(6, 'Head First Python', 'Paul Barry', NULL),
+(7, 'Head First Networking', 'Al Anderson', NULL),
+(8, 'Head First Web Design', 'Ethan Watrall', NULL);
